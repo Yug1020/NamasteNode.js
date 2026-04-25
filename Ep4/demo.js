0e5cf161b8b82823f17df090f9c1da816df32cc6
Yug1020 <yugandhardhore@gmail.com>
2026-04-25 21:00:40 +0530

    started Ep4

---
+++ Ep4/demo.js
@@ -0,0 +1,12 @@
+var info = "We are in demo.js file"
+
+var a = 12
+
+var b = 5
+
+console.log(info)
+console.log(a + b)
+
+const {calculateSum} = require("./sum")
+
+calculateSum(a, b)
---
+++ Ep4/sum.js
@@ -0,0 +1,7 @@
+console.log("we are in sum.js")
+
+function calculateSum(a, b){
+    console.log(a + b)
+}
+
+module.exports = {calculateSum};
