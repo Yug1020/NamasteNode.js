66af6335d8aa23cb707b0cef0cc1460ca1d275d7
Yug1020 <yugandhardhore@gmail.com>
2026-04-26 18:14:48 +0530

    check strict and non-strict behaviour of different modules

--- Ep4/multiple.js
+++ Ep4/multiple.js
@@ -1,5 +1,5 @@
 console.log("We are in multiple")
-
+var z = "Hello world" //follow strict rule. have to assign type of variable before declare.But not in case of require, check "sum.js" module.
 export function multiply(a, b){
     console.log(a*b)
 }
--- Ep4/sum.js
+++ Ep4/sum.js
@@ -1,5 +1,5 @@
 console.log("we are in sum.js")
-
+z = "Hello world" //Commonjs module is non-strict. we can assign var without any type(var, let, const) but not in case of mjs, check multiple.js module.
 function calculateSum(a, b){
     console.log(a + b)
 }
