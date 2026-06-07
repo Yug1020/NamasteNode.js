3d40024aeac906043e3063a37f33b7e3d88dc24e
Yug1020 <yugandhardhore@gmail.com>
2026-06-07 12:40:50 +0530

    suiiiiiiii

--- Ep11/server.js
+++ Ep11/server.js
@@ -4,3 +4,9 @@ http.createServer((req, res) => {
     res.write("write on server:- Hello world\n")
     res.end("end point on server:- tata bye bye")
 }).listen("5507")
+
+
+// command to run
+
+// node server.js
+// then hit a request by opening localhost:5507 on browser then you will see the result on browser
